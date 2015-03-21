9592c2be1e5582de54a914c18bad3d91de67436d
Julia Edwards <jedwardsjunior@gmail.com>
2015-03-21 11:30:07 -0400

    Webapp only

--- api/config.py
+++
@@ -1,8 +0,0 @@
-def getWolframAPIKey():
-  return "25J6UH-L9R2XYAP69"
-
-def getFacebookAppId():
-  return "741682312571388"
-
-def getFacebookAppSecret():
-  return "788d3b32baf843869112a4bc29658b24"
